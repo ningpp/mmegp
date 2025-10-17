95e9de11081968585908f958a79001ef671cb98b
宁培培 <1039725910@qq.com>
2025-10-17 20:16:40 +0800

    add unit test

---
+++ mmegp-mbg-ext/src/test/java/me/ningpp/mmegp/softdelete/SoftDeleteEntity8.java
@@ -0,0 +1,69 @@
+/*
+ *    Copyright 2025 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package me.ningpp.mmegp.softdelete;
+
+import me.ningpp.mmegp.annotations.Column;
+import me.ningpp.mmegp.annotations.SoftDelete;
+import me.ningpp.mmegp.annotations.Table;
+import me.ningpp.mmegp.mybatis.type.uuid.UUIDNoDashStringTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+
+import java.util.UUID;
+
+@Table(table = "SoftDeleteEntity8")
+@SoftDelete(column = "deleted")
+public class SoftDeleteEntity8 {
+    @Column(name = "id", jdbcType = JdbcType.INTEGER, id = true, generatedValue = true)
+    private Integer id;
+    @Column(name = "name", jdbcType = JdbcType.VARCHAR)
+    private String name;
+    @Column(name = "uuid", jdbcType = JdbcType.VARCHAR, typeHandler = UUIDNoDashStringTypeHandler.class)
+    private UUID uuid;
+    @Column(name = "deleted", jdbcType = JdbcType.TINYINT)
+    private Byte deleted;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public Byte getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Byte deleted) {
+        this.deleted = deleted;
+    }
+}
